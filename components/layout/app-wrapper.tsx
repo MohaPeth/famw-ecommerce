80c33e278583fdea15f2eb7cd12ddbf53de33b76
Moha <109183358+MohaPeth@users.noreply.github.com>
2025-06-06 11:26:49 +0000

    Fix hydration error by dynamically importing Preloader

--- components/layout/app-wrapper.tsx
+++ components/layout/app-wrapper.tsx
@@ -1,9 +1,14 @@
 "use client"
 
 import type React from "react"
+import dynamic from "next/dynamic"
 
 import { usePreloader } from "@/hooks/usePreloader"
-import { Preloader } from "@/components/ui/preloader"
+
+// Dynamically import Preloader to prevent hydration mismatch
+const Preloader = dynamic(() => import("@/components/ui/preloader").then(mod => ({ default: mod.Preloader })), {
+  ssr: false
+})
 
 interface AppWrapperProps {
   children: React.ReactNode
